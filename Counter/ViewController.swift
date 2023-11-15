7f60136dd9f336340ddd9677285a5ddc5802c693
Александр Дудченко <Balamut878S@yandex.ru>
2023-11-15 13:06:09 +0300

    Поправил синтаксис,код стал более читабельным,так же поставил модификатор доступа закрытый!

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var incrementButton: UIButton!
-    var counter = 0
+    private var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Ставлю счётчик
         counterLabel.text = "Значение счётчика: \(counter)"
     }
     @IBAction func incrementCounter(_sender: UIButton) {
-            counter += 1
-            counterLabel.text = "Значение счётчика: \(counter)"
-        }
-
-
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    
 }
 
